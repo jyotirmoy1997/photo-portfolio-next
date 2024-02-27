95c954023233553d02bb89ff8d350b609f8185e2
jyotirmoy1997 <subhodas45036@gmail.com>
2024-02-27 18:37:51 +0530

    Optimized Fonts in Gallery Preview : Migrated fonts from CSS imports to NextJS Google Fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,12 +4,20 @@ import Footer from '@/components/footer/footer.component'
 import classes from "./layout.module.css"
 import Image from 'next/image'
 import whatsAppLogo from "@/public/logos/whatsapp.png"
-import { Noto_Sans } from "next/font/google"
+import { Noto_Sans, Cormorant_Garamond } from "next/font/google"
 
 
 const NotoSans = Noto_Sans({
   weight: [ "300", "400", "500", "600", "700" ],
   subsets: [ "latin" ],
+  variable: '--font-noto-sans',
+  display : "swap"
+})
+
+const CormorantGaramond = Cormorant_Garamond({
+  weight: [ "300", "400", "500", "600", "700" ],
+  subsets: [ "latin" ],
+  variable: '--font-cormorant-garamond',
   display : "swap"
 })
 
@@ -22,7 +30,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={NotoSans.className} >
+      <body suppressHydrationWarning={true} className={`${NotoSans.variable} ${CormorantGaramond.variable}`} >
         <Navigation/>
         <div className={classes.wrapper}>
           {children}
